df39bb4669c9e43f7caa3ff091f8eddbc561b21c
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-11-06 22:23:46 -0300

    add base functions and variables

---
+++ main.js
@@ -0,0 +1,10 @@
+let num1 = document.addEventListener('click', )
+let operator = document.addEventListener('click', )
+let num2 = document.addEventListener('click', )
+
+
+const sum = (a, b) => a + b
+const substract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a/b
+
