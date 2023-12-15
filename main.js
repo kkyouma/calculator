34a2629e7aacdce399a7281fbe5c1cf7857da5de
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-12-15 19:22:11 -0300

    Refactor arithmetic operations in main.js

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let num1 = document.addEventListener('click', )
-let operator = document.addEventListener('click', )
-let num2 = document.addEventListener('click', )
+let num1 = document.addEventListener('click', );
+let operator = document.addEventListener('click', );
+let num2 = document.addEventListener('click', );
 
 
-const sum = () => num1 + num2
+const add = () => num1 + num2
 const substract = () => num1 - num2
-const multiply = () => num1 * num2
+const multiply = () => num1 * num2 
 const divide = () => num1 / num2
 
 
 
 function operate (num1, operator, num2) {
-
+  if (operator === '+') {
+    return add(num1, num2)
+  } else if (operator === '-') {
+    return substract(num1, num2)
+  } else if (operator === '*') {
+    return multiply(num1, num2)
+  } else if (operator === '/') {
+    return divide(num1, num2)
+  } else {
+    return 'Invalid operator'
+  }
 }
