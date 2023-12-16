474d7fe3511fb754c18a52e75a56f32a80cbe583
kkyouma <144501527+kkyouma@users.noreply.github.com>
2023-12-16 19:38:01 -0300

    Refactor button event listeners and add operator event listeners

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const substract = (num1, num2) => num1 - num2
 const multiply = (num1, num2) => num1 * num2 
 const divide = (num1, num2) => num1 / num2
 
-
 function operate (num1, operator, num2) {
   if (operator === '+') {
     return add(num1, num2)
@@ -29,20 +28,34 @@ const operators = document.querySelectorAll('.operator')
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     let buttonValue = button.textContent
-    num1 += buttonValue
-    display.textContent = num1
 
-    if (buttonValue === '=') {
-      display.textContent = operate(num1, operator, num2)
-      if (display.textContent === 'Invalid operation') {
-        num1 = '0'
-      }
+    if (!operator) {
+      num1 += buttonValue
+      display.textContent = num1
+    } else if (operator) {
+      num2 += buttonValue
+      display.textContent = num2
     }
 
     if (buttonValue === 'C') {
-      num1 = '0'
-      display.textContent = num1
+      num1 = ''
+      num2 = ''
+      operator = ''
+      display.textContent = ''
+    }
+
+    if (buttonValue === '=') {
+      let result = operate(num1, operator, num2)
+      display.textContent = result
     }
   })
 })
+
+operators.forEach(op => {
+  op.addEventListener('click', () => {
+    operator = op.textContent
+  })
+})
+
+
 
